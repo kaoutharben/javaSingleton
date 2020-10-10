7c10539295752ef348677da7e791b323e3574b89
kaouthar ben seffaj <kaoutharben1998@gmail.com>
2020-10-10 21:43:02 +0200

    Threads implemantatiom

---
+++ src/com/company/com/company/Client.java
@@ -0,0 +1,45 @@
+package com.company;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Client extends Thread {
+    static int  population;
+    static int age;
+
+    @Override
+    public void run() {
+        System.out.println("ENTRER AGE: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            age= Integer.parseInt(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ENTRER LA POPULATION: ");
+        BufferedReader readerPop = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            population= Integer.parseInt(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Terre terre=Terre.getInstance(population,age);
+        System.out.println(Thread.currentThread().getId()+" TERRE: "+ terre);
+        try{
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)  {
+
+        Client client=new Client();
+        client.start();
+        Client client1=new Client();
+        client1.start();
+
+
+    }
+}
